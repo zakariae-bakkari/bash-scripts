eb32ed32cf08f7241b619b0afbea3fa2de8b6340
zakariae-bakkari <zakariabakkari2006@gmail.com>
2025-12-29 21:51:41 +0100

    ajouter exercice5

---
+++ tp3/exercice5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int main() {
+    pid_t p1, p2, fin;
+    int status;
+
+    p1 = fork();
+    if (p1 == 0) {
+        execl("/bin/ls", "ls", "-l", NULL);
+        exit(1);
+    }
+
+    p2 = fork();
+    if (p2 == 0) {
+        execl("/bin/ps", "ps", "-l", NULL);
+        exit(1);
+    }
+
+    fin = wait(&status);
+    printf("Premier processus terminé : PID = %d\n", fin);
+
+    wait(NULL); // attendre le fils 2
+    return 0;
+}
