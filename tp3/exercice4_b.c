56b8807c60f14ce4d01e104d855e80988ce67d49
zakariae-bakkari <zakariabakkari2006@gmail.com>
2025-12-29 21:51:36 +0100

    ajouter exercice4_b

---
+++ tp3/exercice4_b.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int main() {
+    pid_t pid;
+    int status;
+
+    char *args[] = {"ps", "-l", NULL};
+
+    pid = fork();
+
+    if (pid == 0) {
+        execv("/bin/ps", args);
+        perror("Erreur execv");
+        exit(1);
+    }
+    else {
+        wait(&status);
+        printf("Code retour de ps : %d\n", WEXITSTATUS(status));
+    }
+
+    return 0;
+}
