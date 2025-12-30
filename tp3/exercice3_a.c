0a58b31261edf226b3a67cf35b0e8861144dcdfc
zakariae-bakkari <zakariabakkari2006@gmail.com>
2025-12-30 17:11:25 +0100

    ajouter l'inclusion de <errno.h> et corriger le retour du processus fils

--- tp3/exercice3_a.c
+++ tp3/exercice3_a.c
@@ -4,6 +4,7 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <time.h>
+#include <errno.h>
 
 int main() {
     pid_t p;
@@ -13,16 +14,22 @@ int main() {
     if (p == 0) { // fils
         srand(time(NULL) ^ getpid()); // pour eviter la meme graine
         int n = rand() % 101;
-        exit(n);
+        sleep(2000); // pour simuler un traitement long
+        return n;
     } 
     else {
         int status;
-        waitpid(p, &status, 0);
-
-        if (WIFEXITED(status)) {
+        waitpid(p, &status, 0);// status recupere le status de termiison
+        printf("status: %d\n", status);
+        if (WIFEXITED(status))
+        {
             int ret = WEXITSTATUS(status);
             printf("Le fils a termine avec le code: %d\n", ret);
         }
+        else if (WIFSIGNALED(status)){
+            printf("Le fils a ete tue par le signal: %d\n", WTERMSIG(status));
+        }
+        
     }
 
     return 0;
