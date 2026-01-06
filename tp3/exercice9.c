0311a8ae11f19fa53e135e6258ec1b209f90c471
zakariae-bakkari <zakariabakkari2006@gmail.com>
2026-01-06 13:29:51 +0100

    Refactor command parsing and execution in main shell loop

--- tp3/exercice9.c
+++ tp3/exercice9.c
@@ -5,6 +5,36 @@
 #include <sys/wait.h>
 #include <string.h>
 
+char **separer_commande(char commande[])
+{
+   int taille = strlen(commande);
+   char **table_commande = malloc(100 * sizeof(char *));
+   int indice = 0;
+   char mot[256];
+   int mot_index = 0;
+
+   for (int i = 0; i <= taille; i++)
+   {
+      if (commande[i] != ' ' && commande[i] != '\0')
+      {
+         mot[mot_index++] = commande[i];
+      }
+      else
+      {
+         if (mot_index > 0)
+         {
+            mot[mot_index] = '\0';
+            table_commande[indice] = malloc((mot_index + 1) * sizeof(char));
+            strcpy(table_commande[indice], mot);
+            indice++;
+            mot_index = 0;
+         }
+      }
+   }
+   table_commande[indice] = NULL; // Null-terminate the array
+   return table_commande;
+}
+
 int main()
 {
    char commande[256];
@@ -12,21 +42,28 @@ int main()
    do
    {
       printf("monshell> ");
-      scanf("%255s", commande);
+      scanf(" %255[^\n]", commande);
       if (strcmp(commande, "exit") == 0)
       {
          exit(-1);
       }
+      char **table_commande = separer_commande(commande);
 
       pid = fork();
       if (pid == 0)
       {
-         if (execlp(commande, commande, NULL) == -1)
-         {
-            printf("salam");
-         }
+         execvp(table_commande[0], table_commande);
+         perror("execlp");
+         exit(-1);
       }
       wait(NULL);
+
+      
+      for (int i = 0; table_commande[i] != NULL; i++)
+      {
+         free(table_commande[i]);
+      }
+      free(table_commande);
    } while (1);
 
    return 0;
