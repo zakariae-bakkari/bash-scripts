1c67caeabf3b8da7841467976d64113207440abf
zakariae-bakkari <zakariabakkari2006@gmail.com>
2026-01-20 09:06:30 +0100

    Refactor thread functions for improved readability and consistency

--- tp4/ex4_1_2.c
+++ tp4/ex4_1_2.c
@@ -3,28 +3,41 @@
 #include <stdlib.h>
 
 // Partie 4.1 : Alphabet
-void* thread_min(void* arg) {
-    for(char c='a'; c<='z'; c++) printf("%c", c);
+void *thread_min(void *arg)
+{
+    for (char c = 'a'; c <= 'z'; c++)
+        printf("%c", c);
+    printf("\n");
     return NULL;
 }
-void* thread_maj(void* arg) {
-    for(char c='A'; c<='Z'; c++) printf("%c", c);
+void *thread_maj(void *arg)
+{
+    for (char c = 'A'; c <= 'Z'; c++)
+        printf("%c", c);
+    printf("\n");
     return NULL;
 }
 
 // Partie 4.2 : Écriture fichier
-FILE *fichier; // Ressource partagée 
+FILE *fichier; // Ressource partagée
 
-void* ecrire_1(void* arg) {
-    for(int i=0; i<=100000; i++) fprintf(fichier, "%d\n", i);
+void *ecrire_1(void *arg)
+{
+    for (int i = 0; i <= 100000; i++)
+        fprintf(fichier, "%d\n", i);
+    printf("\n");
     return NULL;
 }
-void* ecrire_2(void* arg) {
-    for(int i=1000000; i<=1100000; i++) fprintf(fichier, "%d\n", i);
+void *ecrire_2(void *arg)
+{
+    for (int i = 1000000; i <= 1100000; i++)
+        fprintf(fichier, "%d\n", i);
+    printf("\n");
     return NULL;
 }
 
-int main() {
+int main()
+{
     pthread_t t1, t2;
 
     // --- Exec 4.1 ---
@@ -42,6 +55,6 @@ int main() {
     pthread_join(t2, NULL);
     fclose(fichier);
     printf("Fichier écrit. Ouvrez output.txt pour voir le mélange !\n");
-    
+
     return 0;
 }
